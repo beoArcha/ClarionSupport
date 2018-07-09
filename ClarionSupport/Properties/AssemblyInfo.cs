7006c4095849d519a29662c78604e797d5817b17
marcin <b*****a@gmail.com>
2018-07-09 08:45:57 +0200

    assembly info update

--- ClarionSupport/Properties/AssemblyInfo.cs
+++ ClarionSupport/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Marcin Stefański")]
 [assembly: AssemblyProduct("C6Free Companion App")]
-[assembly: AssemblyCopyright("Copyright ©  2017 The MIT License")]
+[assembly: AssemblyCopyright("Copyright ©  2017, 2018 The MIT License")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.3.0")]
+[assembly: AssemblyVersion("1.3")]
 [assembly: AssemblyFileVersion("1.1.2")]
